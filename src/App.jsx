5aaab936648887d2afd98e051bcf09d1b88ddacc
Ravi Kant <kumarra927@gmail.com>
2025-04-07 13:50:20 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import UploadResume from "./components/Pages/UploadResume"
+
+function App() {
+  return (
+    <>
+      <UploadResume/>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Pages/UploadResume.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { Button } from '../ui/button'
+
+const UploadResume = () => {
+  return (
+    <div>
+        <div>
+            <h1 className='bg-red-300'>Upload Resume</h1>
+            <Button>ss</Button>
+        </div>
+    </div>
+  )
+}
+
+export default UploadResume
